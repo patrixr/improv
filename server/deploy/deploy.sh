5221023ad78e68f2054cd0d60d7fadeb11dd2c65
Patrick R <patrick.rabier@gmail.com>
2016-09-19 10:23:35 +0000

    FIX: deploy script

--- server/deploy/deploy.sh
+++ server/deploy/deploy.sh
@@ -5,13 +5,14 @@ set -e
 NAME="improv"
 GO_PATH="$GOPATH"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN_DIR=$(realpath "$DIR/..")
 BIN="server"
 SERVICE_FILE="$DIR/$NAME.service"
 
 sudo apt-get update
 sudo apt-get install sox
 
-sudo sh -c "sed -e 's;%DIR%;$DIR;g' -e 's;%BIN%;$BIN;g' $SERVICE_FILE > /etc/systemd/system/$NAME.service"
+sudo sh -c "sed -e 's;%DIR%;$BIN_DIR;g' -e 's;%BIN%;$BIN;g' $SERVICE_FILE > /etc/systemd/system/$NAME.service"
 
 sudo systemctl enable $NAME
 sudo systemctl start $NAME
